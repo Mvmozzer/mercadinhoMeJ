2870e146129af87e03dafac927fea3a2e6d9d8ee
Marcos Vinicius Mozzer <marcosviniciusmozzer@gmail.com>
2026-07-12 11:22:15 -0300

    Atualiza lojinha automaticamente (ajuste_estoque)

--- miniapp/src/catalog.js
+++ miniapp/src/catalog.js
@@ -1,4 +1,4 @@
-import { slugify } from './utils.js?v=2026.07.12.122';
+import { slugify } from './utils.js?v=2026.07.12.133';
 
 const WEIGHTED_CATALOG_MARKERS = ['item.tarjas'];
 export const WHOLESALE_DEFAULTS = {
