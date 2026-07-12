80ad938bd5239cda21df13c5c328206ac4e72267
Marcos Vinicius Mozzer <marcosviniciusmozzer@gmail.com>
2026-07-12 20:25:51 -0300

    Atualiza lojinha automaticamente (promocao_produto)

--- miniapp/src/cart.js
+++ miniapp/src/cart.js
@@ -1,5 +1,5 @@
-import { CART_KEY, readJson, writeJson } from './storage.js?v=2026.07.12.687';
-import { productAvailability, productWholesale } from './catalog.js?v=2026.07.12.687';
+import { CART_KEY, readJson, writeJson } from './storage.js?v=2026.07.12.747';
+import { productAvailability, productWholesale } from './catalog.js?v=2026.07.12.747';
 
 function itemQuantity(item = {}) {
   const quantity = Number(item.quantity ?? item.quantidade ?? item.qtd ?? 0);
--- miniapp/src/state.js
+++ miniapp/src/state.js
@@ -1,5 +1,5 @@
-﻿import { restoreMiniAppUiState } from './storage.js?v=2026.07.12.687';
-import { normalizeWholesaleConfig } from './catalog.js?v=2026.07.12.687';
+﻿import { restoreMiniAppUiState } from './storage.js?v=2026.07.12.747';
+import { normalizeWholesaleConfig } from './catalog.js?v=2026.07.12.747';
 
 export const MINIAPP_UI_DEFAULTS = {
   header: {
