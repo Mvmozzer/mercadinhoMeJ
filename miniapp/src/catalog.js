b53a55a696153c78591c36028d5ee057be9694ad
Marcos Vinicius Mozzer <marcosviniciusmozzer@gmail.com>
2026-07-12 21:29:41 -0300

    Atualiza app publico para tunel local

--- miniapp/src/catalog.js
+++ miniapp/src/catalog.js
@@ -1,4 +1,4 @@
-import { slugify } from './utils.js?v=2026.07.12.783';
+import { slugify } from './utils.js?v=2026.07.12.576';
 
 const WEIGHTED_CATALOG_MARKERS = ['item.tarjas'];
 export const WHOLESALE_DEFAULTS = {
--- miniapp/src/state.js
+++ miniapp/src/state.js
@@ -1,5 +1,5 @@
-﻿import { restoreMiniAppUiState } from './storage.js?v=2026.07.12.783';
-import { normalizeWholesaleConfig } from './catalog.js?v=2026.07.12.783';
+﻿import { restoreMiniAppUiState } from './storage.js?v=2026.07.12.576';
+import { normalizeWholesaleConfig } from './catalog.js?v=2026.07.12.576';
 
 export const MINIAPP_UI_DEFAULTS = {
   header: {
